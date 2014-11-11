22f3cff995fb3f43da6b386bec7ff8b21fdcda1b
Diogo Eiras <eiras@outlook.com>
2014-11-11 12:32:04 +0000

    Versão inicial do começo do jogo

--- 16_!.pl
+++ 16_!.pl
@@ -1,4 +1,5 @@
 :- use_module(library(lists)).
+todas_pecas([[[1,1,1],[1,0,1],[1,1,1]],[[1,1,1],[1,0,0],[1,1,1]],[[1,1,1],[1,0,1],[0,1,1]],[[1,1,1],[1,0,0],[1,0,1]],[[1,1,1],[1,0,1],[0,0,0]],[[1,1,1],[0,0,1],[0,1,1]],[[1,1,1],[1,0,0],[1,1,0]],[[1,1,1],[1,0,0],[1,0,0]],[[1,1,1],[1,0,0],[0,0,0]],[[1,1,1],[0,0,1],[0,0,0]],[[1,1,1],[0,0,0],[0,0,0]],[[0,1,1],[0,0,0],[0,0,0]],[[1,1,0],[0,0,0],[0,0,0]],[[1,0,0],[0,0,0],[0,0,0]],[[0,1,0],[0,0,0],[0,0,0]]]).
 peca([[1,1,1],[1,0,1],[1,1,1]]). 
 peca([[1,1,1],[1,0,0],[1,1,1]]). 
 peca([[1,1,1],[1,0,1],[0,1,1]]). 
@@ -16,7 +17,151 @@ peca([[1,1,0],[0,0,0],[0,0,0]]).
 peca([[1,0,0],[0,0,0],[0,0,0]]). 
 peca([[0,1,0],[0,0,0],[0,0,0]]). 
 
-/*Funciona mas há aquele stresse dos parenteses*/
+start:-
+	imprime_titulo,
+	imprime_opcoes,
+	ler_opcao(Opcao),
+	write('============================'),nl,
+	iniciar_jogo(Opcao).
+
+imprime_titulo:-
+	write('========= Jogo 16! ========='),
+	nl.
+
+imprime_opcoes:-
+	write('|1 - Jogador contra jogador|'),
+	nl.
+
+opcao_valida_menu(1).
+
+ler_opcao(Opcao):-
+	write('Escolha uma das opcoes do menu'),
+	nl,
+	write('> '),
+	read(Opcao_aux),
+	(opcao_valida_menu(Opcao_aux)->Opcao is Opcao_aux;ler_opcao(Opcao)).
+
+/* Jogador vs Jogador */
+iniciar_jogo(1):-
+	cria_tabuleiro(4,Tabuleiro),
+	todas_pecas(Pecas),
+	jogar(1,Pecas,[],Tabuleiro).
+
+jogar(1,PecasRestantes,PecasJogadas,Tabuleiro):-
+	write('Joga o jogador 1'),nl,
+	imprime_tabuleiro(Tabuleiro),
+	write('Escolha a peca a jogar'),nl,
+	imprime_varias_pecas(1,PecasRestantes).
+	
+imprime_varias_pecas(_,[]).
+
+imprime_varias_pecas(N,[H|T]):-
+	write(N),
+	write(':-'),nl,
+	write('|'),imprime_parte_peca(H,0,0),nl,
+	write('|'),imprime_parte_peca(H,1,0),nl,
+	write('|'),imprime_parte_peca(H,2,0),nl,nl,
+	Next is N + 1,
+	imprime_varias_pecas(Next,T).
+
+
+imprime_linhas(_,[]).
+
+imprime_linhas(N,[LinhaTabuleiro|RestoTabuleiro]):-
+		imprime_linha(0,LinhaTabuleiro,LinhaTabuleiro),
+		N1 is N + 1,
+		imprime_linhas(N1,RestoTabuleiro).
+
+imprime_parte_a_imprimir([]):-
+		write('|').
+
+
+imprime_parte_a_imprimir([0]):-
+		write(' '),
+		imprime_parte_a_imprimir([]).
+
+imprime_parte_a_imprimir([1]):-
+		write('X'),
+		imprime_parte_a_imprimir([]).
+
+imprime_parte_a_imprimir([0|Resto]):-
+		write('  '),
+		imprime_parte_a_imprimir(Resto).
+
+imprime_parte_a_imprimir([1|Resto]):-
+		write('X '),
+		imprime_parte_a_imprimir(Resto).
+
+
+imprime_parte_peca([ParteImprimir|_],N,N):-
+		imprime_parte_a_imprimir(ParteImprimir).
+
+imprime_parte_peca([_|Resto],N,Pos):-
+		Pos < 2,
+		Pos1 is Pos + 1,
+		imprime_parte_peca(Resto,N,Pos1).
+
+imprime_linha(3,_,_):-
+		nl.	
+
+imprime_linha(N,[],Original):-
+		nl,
+		N1 is N+1,
+		imprime_linha(N1,Original,Original).
+
+imprime_linha(N,[Peca|RestoLinha],Original):-
+		integer(N),
+		N >= 0,
+		write('|'),
+		imprime_parte_peca(Peca,N,0),
+		write(' '),
+		imprime_linha(N,RestoLinha,Original).
+
+imprime_tabuleiro(Tabuleiro):-
+		nl,
+		imprime_linhas(0,Tabuleiro).
+
+imprime_tabuleiro_exemplo:-
+		nl,
+		imprime_linhas(0,[
+		   [[[0,1,0],[0,0,0],[0,0,0]],[[1,0,0],[0,0,0],[0,0,0]],[[1,1,0],[0,0,0],[0,0,0]],[[0,1,1],[0,0,0],[0,0,0]]],
+		   [[[1,1,1],[0,0,0],[1,1,1]],[[1,0,0],[0,1,1],[0,0,0]],[[1,1,0],[0,1,1],[0,0,0]],[[0,1,1],[0,1,1],[0,0,0]]],
+		   [[[0,1,0],[0,1,1],[0,0,0]],[[1,0,0],[0,0,0],[0,1,1]],[[0,1,1],[0,0,0],[0,1,1]],[[0,1,1],[0,0,0],[0,1,1]]],
+		   [[[0,1,0],[0,0,0],[0,0,0]],[[1,0,0],[0,0,0],[0,0,0]],[[1,1,0],[0,0,0],[0,0,0]],[[0,1,1],[0,1,1],[0,1,1]]]
+		  ]).
+
+adicionar_parte_rodada([Inserir|Resto],Linha1,Linha2,Linha3,0,A,B,C):-
+		append([Inserir],Linha1,A),
+		adicionar_parte_rodada(Resto,Linha1,Linha2,Linha3,1,A,B,C).
+
+adicionar_parte_rodada([Inserir|Resto],Linha1,Linha2,Linha3,1,A,B,C):-
+		append([Inserir],Linha2,B),
+		adicionar_parte_rodada(Resto,Linha1,Linha2,Linha3,2,A,B,C).
+
+adicionar_parte_rodada([Inserir|Resto],Linha1,Linha2,Linha3,2,A,B,C):-
+		append([Inserir],Linha3,C),
+		adicionar_parte_rodada(Resto,Linha1,Linha2,Linha3,3,A,B,C).
+
+adicionar_parte_rodada(_,_,_,_,3,_,_,_).
+
+rodar_peca_direita(Peca,PecaFinal):-
+		rodar_peca_direita_aux(Peca,0,[],[],[],PecaFinal).
+
+rodar_peca_direita_aux([],0,[],[],[],_).
+
+rodar_peca_direita_aux(_,3,A,B,C,PecaFinal):-
+		append([A],[B],L),
+		append(L,[C],PecaFinal),
+		rodar_peca_direita_aux([],0,[],[],[],PecaFinal).
+
+rodar_peca_direita_aux([Parte|Resto],Linha,Linha1,Linha2,Linha3,PecaFinal):-
+		integer(Linha),
+		Linha >= 0,
+		Linha < 3,
+		adicionar_parte_rodada(Parte,Linha1,Linha2,Linha3,0,A,B,C),
+		LinhaSegue is Linha + 1,
+		rodar_peca_direita_aux(Resto,LinhaSegue,A,B,C,PecaFinal).
+
 joga_peca(Peca, Tabuleiro, X ,Y, Final):-
 			insere_peca(Peca, Tabuleiro, X ,Y, Final).
 
